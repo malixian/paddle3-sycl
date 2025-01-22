3193034c9c0f2e4b9b5e369f0a9c3110edfce020
malixian <malixian@ict.ac.cn>
2025-01-22 03:30:27 +0000

    add reduce_sum, reduce_min/max to deny op

--- paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
+++ paddle/cinn/hlir/dialect/operator/transforms/pd_to_cinn_pass.cc
@@ -1154,11 +1154,13 @@ pir::RewritePatternSet PdOpToCinnOpPass::InitializePatterns(
   pir::RewritePatternSet ps(context);
   ps.Add<ScaleOpPattern>(
       context);  // NOTE, scale op pattern should before AddBroadcastTo
+  /*
   ps.Add<SumOpPattern>(context);
   ps.Add<ReduceMinMaxOpPattern<paddle::dialect::MinOp,
                                cinn::dialect::ReduceMinOp>>(context);
   ps.Add<ReduceMinMaxOpPattern<paddle::dialect::MaxOp,
                                cinn::dialect::ReduceMaxOp>>(context);
+  */
   ps.Add<ProdOpPattern>(context);
   ps.Add<ReshapeOpPattern>(context);
   ps.Add<PowOpPattern>(context);
